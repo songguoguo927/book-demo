187c1671b2a30baeed1995ed1435296ce62a6372
songguoguo927 <1351443467@qq.com>
2019-01-08 22:25:27 +0800

    view层

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,18 +1,29 @@
 var express = require('express');
-
 var app = express();
+//设置 handlebars 视图引擎
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-app.set('port',precess.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-//定制404页面
-app.use(function(req,res){
-	res.type('text/plain');
-	res.status(404);
-	res.send('404-not found');
+//给首页和关于页面加上路由。
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
 });
-
-//定制500页面
-app.use(function(err,req,res,next){
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+// 定制 404 页面
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+// 定制 500 页面
+app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('text/plain');
     res.status(500);
