5c3094f31d8b0d7bcdef191fa6a4988ec9d60840
songguoguo927 <1351443467@qq.com>
2019-01-08 22:28:55 +0800

    使用模板引擎后路由替换

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -7,27 +7,47 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+//旧路由
 //给首页和关于页面加上路由。
-app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-});
-app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-});
+// app.get('/', function(req, res){
+//     res.type('text/plain');
+//     res.send('Meadowlark Travel');
+// });
+// app.get('/about', function(req, res){
+//     res.type('text/plain');
+//     res.send('About Meadowlark Travel');
+// });
+
 // 定制 404 页面
-app.use(function(req, res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+// app.use(function(req, res){
+//     res.type('text/plain');
+//     res.status(404);
+//     res.send('404 - Not Found');
+// });
+// // 定制 500 页面
+// app.use(function(err, req, res, next){
+//     console.error(err.stack);
+//     res.type('text/plain');
+//     res.status(500);
+//     res.send('500 - Server Error');
+// });
+//新路由
+app.get('/', function(req, res) {
+    res.render('home');
 });
-// 定制 500 页面
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+// 404 catch-all 处理器（中间件）
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+// 500 错误处理器（中间件）
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
