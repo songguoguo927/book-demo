34d4c6c4dce988fe0b25fc841e502fb55e92d921
songguoguo927 <1351443467@qq.com>
2019-01-09 17:03:56 +0800

    gitignore文件

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 var fortune = require('./lib/fortune.js');
 //设置 handlebars 视图引擎
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express3-handlebars').create({
+        defaultLayout:'main',
+
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    },
+});
 // ,extname:'.hbs'
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -21,6 +31,13 @@ app.use(require('body-parser')());
 app.get('/newsletter', function(req, res){
 // 我们会在后面学到 CSRF……目前，只提供一个虚拟值
     res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
 });
 //旧路由
 //给首页和关于页面加上路由。
